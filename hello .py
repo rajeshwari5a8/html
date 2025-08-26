5efa7ac6404fb61d52919d439d191e558d184cb5
rajeshwari5a8 <orsurajeshwari11@gmail.com>
2025-08-26 11:49:29 +0530

    Create hello .py

---
+++ hello .py
@@ -0,0 +1,11 @@
+# hello_github.py
+
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print("Hello, GitHub!")
+    x = 5
+    y = 7
+    result = add_numbers(x, y)
+    print(f"The sum of {x} and {y} is {result}")
